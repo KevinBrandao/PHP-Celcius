89af2f9388701876a2e8a6d6eac5e3b32ffb8b04
KevinBrandao <kevenbrandaoms@outlook.com>
2022-01-12 19:50:18 -0300

    PHP Celcius

---
+++ conversor.php
@@ -0,0 +1,8 @@
+<?php
+
+$form1 = $_POST['temp'];
+
+$formula1 = ($from1 -32) * 5/9;
+
+    echo $formula1 . '°C';
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor</title>
+</head>
+<body>
+    <form action="conversor.php" method="POST" >
+   
+    <input type="number" name="temp" placeholder="Fahrenheit:">
+    <button type="submit">Conversor</button>
+
+</form>
+</body>
+</html>
